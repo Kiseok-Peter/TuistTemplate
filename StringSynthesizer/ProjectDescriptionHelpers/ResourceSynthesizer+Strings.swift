5ec30feab598d339b5b096376d92686a93b843f0
박기석 <gkfnrkfl@naver.com>
2023-12-15 09:38:17 +0900

    feat: ResourceSynthesizer에 플러그인 설정 strings 추가

---
+++ StringSynthesizer/ProjectDescriptionHelpers/ResourceSynthesizer+Strings.swift
@@ -0,0 +1,14 @@
+//
+//  ResourceSynthesizer+Strings.swift
+//  StringSynthesizer
+//
+//  Created by 박기석 on 12/15/23.
+//
+
+import ProjectDescription
+
+public extension ResourceSynthesizer {
+    static var strings: Self {
+        .strings(plugin: "StringSynthesizer")
+    }
+}
