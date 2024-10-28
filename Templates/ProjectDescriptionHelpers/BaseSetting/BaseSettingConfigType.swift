8fe5e4424fd43a338cc542349562f2cbe8595661
박기석 <gkfnrkfl@naver.com>
2024-10-28 19:03:45 +0900

    feat: 기본 세팅 관련 Config 추가

---
+++ Templates/ProjectDescriptionHelpers/BaseSetting/BaseSettingConfigType.swift
@@ -0,0 +1,13 @@
+//
+//  BaseSettingConfigType.swift
+//  Templates
+//
+//  Created by 박기석 on 10/28/24.
+//
+
+import Foundation
+
+public protocol BaseSettingConfigType {
+    var organizationName: String { get }
+    var iOSVersion: String { get }
+}
---
+++ Templates/ProjectDescriptionHelpers/BaseSetting/DefaultBaseSettingConfig.swift
@@ -0,0 +1,13 @@
+//
+//  DefaultBaseSettingConfig.swift
+//  Templates
+//
+//  Created by 박기석 on 10/28/24.
+//
+
+import Foundation
+
+public struct DefaultBaseSettingConfig: BaseSettingConfigType {
+    public var organizationName: String { "kr.kiseok" }
+    public var iOSVersion: String { "16.0" }
+}
